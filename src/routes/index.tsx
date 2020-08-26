c3d9a5b7555366caf0818d734b3b2dd136e398e6
Matheus Oliveira da Hora <36653020+MatheusDev-1@users.noreply.github.com>
2020-08-26 11:19:42 -0300

    Aplicação finalizada

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,9 +5,10 @@ import Dashboard from '../pages/Dashboard';
 import Repository from '../pages/Repository';
 
 const Routes: React.FC = () => (
+  // O "+" é para indicar que a '/' depois de repositories/ também deve ser usada
   <Switch>
     <Route path="/" exact component={Dashboard} />
-    <Route path="/repository" component={Repository} />
+    <Route path="/repositories/:repository+" component={Repository} />
   </Switch>
 );
 
