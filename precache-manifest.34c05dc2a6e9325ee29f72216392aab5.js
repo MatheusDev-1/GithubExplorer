32b75dd6c9a2ba743177baff806d01b6182383e8
Matheus Oliveira da Hora <36653020+MatheusDev-1@users.noreply.github.com>
2020-08-26 00:17:13 -0300

    Updates

---
+++ precache-manifest.34c05dc2a6e9325ee29f72216392aab5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14d7c97fc191ab8124c842dbc104603e",
+    "url": "/GithubExplorer/index.html"
+  },
+  {
+    "revision": "916ad6a1413b9a990b19",
+    "url": "/GithubExplorer/static/js/2.42383ec8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/GithubExplorer/static/js/2.42383ec8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dd810113ebec9d23433",
+    "url": "/GithubExplorer/static/js/main.86cf0223.chunk.js"
+  },
+  {
+    "revision": "1bd00cd3e24130898c59",
+    "url": "/GithubExplorer/static/js/runtime-main.4eff6574.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/GithubExplorer/static/media/githubbackground.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/GithubExplorer/static/media/logo.04f6798f.svg"
+  }
+]);
